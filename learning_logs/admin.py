bba718bff05f9d1b8a19ae13becba484ac6bb585
Gabriel Rodrigues <souogabrieel@gmail.com>
2023-02-14 15:02:52 -0300

    Topic and entry models defined.

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ["title", "public", "date_created", "owner"]
+    list_filter = ["public", "date_created", "owner"]
+    search_fields = ["title"]
+
+
+@admin.register(models.Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ["topic", "content", "date_created"]
+    list_filter = ["topic", "date_created"]
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=250)
+    public = models.BooleanField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, related_name="topics", on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["title", "owner"], name="Unique title")
+        ]
+        ordering = ["-date_created"]
+
+    def __str__(self):
+        return f"{self.title} - {self.owner}"
+
+
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic, related_name="entries", on_delete=models.CASCADE)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-date_created"]
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        if len(self.content) > 30:
+            return self.content[:30] + "..."
+        else:
+            return self.content
